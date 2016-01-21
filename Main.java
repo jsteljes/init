ed56e348721b8772eae4cc91c3da17159c5082b3
jsteljes <jonathansteljes@Jonathans-MacBook-Air-2.local>
2016-01-20 22:25:47 -0500

    Committing main.java
    Initial commit!

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    
+    private String file = "";
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Starting program!");
+        
+        printHello();
+        
+    }
+    
+    private void printHello() {
+        
+        System.out.println("Hello World!");
+        
+    }
+    
+}
